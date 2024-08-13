3ba8f2978c601f5b6d0ee65e66df2c934f9b62d1
anderson.gomes <gomedanderson@gmail.com>
2024-08-13 09:13:26 -0300

    classse contato

--- src/main/java/br/dcx/ufpb/anderson/agenda/Contato.java
+++ src/main/java/br/dcx/ufpb/anderson/agenda/Contato.java
@@ -1,9 +1,35 @@
 package br.dcx.ufpb.anderson.agenda;
 
+import java.util.Objects;
+
 public class Contato {
     private String nome;
-    private int
+    private int diaAniversario;
+    private int mesAnivesario;
 
+    public Contato(String nome, int diaAniversario, int mesAnivesario){
+        this.nome = nome;
+        this.diaAniversario = diaAniversario;
+        this.mesAnivesario = mesAnivesario;
+    }
 
+    @Override
+    public String toString() {
+        return "Contato de nome " +
+                ", que aniversaria em " + diaAniversario +
+                "/" + mesAnivesario;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return diaAniversario == contato.diaAniversario && mesAnivesario == contato.mesAnivesario && Objects.equals(nome, contato.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, diaAniversario, mesAnivesario);
+    }
 }
