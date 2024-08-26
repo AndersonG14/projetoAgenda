57adbd0d73b1e8564517d4ab5ef1c7e5131c1453
Anderson Gomes <gomedanderson@gmail.com>
2024-08-26 16:51:38 -0300

    adicionando código

--- src/main/java/br/dcx/ufpb/anderson/agenda/AgendaAyla.java
+++ src/main/java/br/dcx/ufpb/anderson/agenda/AgendaAyla.java
@@ -1,11 +1,12 @@
 package br.dcx.ufpb.anderson.agenda;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Collection;
 import java.util.List;
 
-public class AgendaAyla {
+public class AgendaAyla implements Agenda{
     private Map<String, Contato> contatos;
     private GravadorDeDados gravador;
 
@@ -14,13 +15,40 @@ public class AgendaAyla {
         this.gravador = new GravadorDeDados();
     };
 
-    public void salvarDados(){
 
+    @Override
+    public boolean cadastraContato(String nome, int dia, int mes) {
+        if (contatos.containsKey(nome)) {
+            return false; // Contato já existente
+        }
+        contatos.put(nome, new Contato(nome, dia, mes));
+        return true;
     }
 
-    public boolean cadastraContato(String nome, int dia, int mes){
-        Contato c = new Contato(nome, dia, mes);
-        for ()
+    @Override
+    public Collection<Contato> pesquisaAniversariantes(int dia, int mes) {
+        return contatos.values().stream()
+                .filter(c -> c.getDiaAniversario() == dia && c.getMesAniversario() == mes)
+                .toList();
+    }
+
+    @Override
+    public boolean removeContato(String nome) throws ContatoInexistenteException {
+        if (!contatos.containsKey(nome)) {
+            throw new ContatoInexistenteException("Contato não existe: " + nome);
+        }
+        contatos.remove(nome);
+        return true;
+    }
+
+    @Override
+    public void salvarDados() throws IOException {
+        gravador.salvarContatos(contatos);
+    }
+
+    @Override
+    public void recuperarDados() throws IOException {
+        contatos = gravador.recuperarContatos();
     }
 
 }
