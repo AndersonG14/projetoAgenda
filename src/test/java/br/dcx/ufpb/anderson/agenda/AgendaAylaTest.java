89e5241905f1be973b072332474f70d250fc2779
Anderson Gomes <gomedanderson@gmail.com>
2024-08-26 17:19:18 -0300

    adicionando código

--- src/test/java/br/dcx/ufpb/anderson/agenda/AgendaAylaTest.java
+++ src/test/java/br/dcx/ufpb/anderson/agenda/AgendaAylaTest.java
@@ -3,11 +3,27 @@ package br.dcx.ufpb.anderson.agenda;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AgendaAylaTest {
     @Test
     public void testaCadastroPesquisaRemocao(){
         AgendaAyla agenda = new AgendaAyla();
+        Collection<Contato> contatosAchados = agenda.pesquisaAniversariantes(17,11);
+        assertTrue(contatosAchados.size()==0);
+        agenda.cadastraContato("Ana", 11,3);
+        agenda.cadastraContato("Eunice", 14,7);
+        contatosAchados = agenda.pesquisaAniversariantes(20,8);
+        assertEquals(3, contatosAchados.size());
+        Contato ana = new Contato("Ana", 11,3);
+        assertTrue(contatosAchados.contains(ana));
+        Contato eunice = new Contato("Eunice", 14, 7);
+        try {
+            boolean removeu = agenda.removeContato("Eunice");
+            assertTrue(removeu);
+        } catch (ContatoInexistenteException e){
+            fail("Lançou exceção quando não deveria");
+        }
     }
 
 }
